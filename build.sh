db259caa93f4fbe6c25c5d6bd926d7f50158a348
Robert Hailey <rhailey@allogy.com>
2015-10-08 12:19:05 -0500

    f: build.sh installs too

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ if $(dirname $0)/release.sh --build-needed ; then
 	echo "WARNING: not on an official release tag"
 	mvn package
 else
-	TZ=UTC mvn -Drelease.version=${VERSION} clean package install
+	#NB: 'install' implies 'package'
+	TZ=UTC mvn -Drelease.version=${VERSION} clean install
 fi
 
